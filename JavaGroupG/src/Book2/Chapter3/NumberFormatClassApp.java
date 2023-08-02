7769cd1db86e77b0097377443c3b3b6d10ba8854
Nashy1 <nashnyembe0902@gmail.com>
2023-08-02 11:16:05 +0200

    com

--- JavaGroupG/src/Book2/Chapter3/NumberFormatClassApp.java
+++ JavaGroupG/src/Book2/Chapter3/NumberFormatClassApp.java
@@ -5,6 +5,15 @@ public class NumberFormatClassApp {
    static NumberFormat cf = NumberFormat.getCurrencyInstance();
 
     public static void main(String[] args) {
+        printMyAllowance();
+        printCostOfPainBallGun();
+    }
+    public static void printMyAllowance(){
+        double myAllowance = 10.00;
+        cf = NumberFormat.getCurrencyInstance();
+        System.out.println("My Allowance: " + cf.format(myAllowance));
+    }
+    public static void  printCostOfPainBallGun(){
         
     }
 }
