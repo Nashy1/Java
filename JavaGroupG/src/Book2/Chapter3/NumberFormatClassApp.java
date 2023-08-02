dad328309fd08580b26950f974b1f7b3f4568673
Nashy1 <nashnyembe0902@gmail.com>
2023-08-02 11:12:36 +0200

    com

--- JavaGroupG/src/Book2/Chapter3/NumberFormatClassApp.java
+++ JavaGroupG/src/Book2/Chapter3/NumberFormatClassApp.java
@@ -2,5 +2,9 @@ package Book2.Chapter3;
 
 import java.text.NumberFormat;
 public class NumberFormatClassApp {
-   
+   static NumberFormat cf = NumberFormat.getCurrencyInstance();
+
+    public static void main(String[] args) {
+        
+    }
 }
