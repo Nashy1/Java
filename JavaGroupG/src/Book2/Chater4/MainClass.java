3f6476ab8d2b64a2096e4dab28df46e89b92bffd
Nashy1 <nashnyembe0902@gmail.com>
2023-08-02 12:25:45 +0200

    com

--- JavaGroupG/src/Book2/Chater4/MainClass.java
+++ JavaGroupG/src/Book2/Chater4/MainClass.java
@@ -2,8 +2,12 @@ package Book2.Chater4;
 
 public class MainClass {
     public static void main(String[] args) {
+        int appleCount = 5;
         String msg = "You have " + appleCount + " apple"
                 + ((appleCount>1) ? "s." : ".");
         System.out.println(msg);
+
+        if (answer == "Yes")
+            System.out.println("The answer is Yes.");
     }
 }
