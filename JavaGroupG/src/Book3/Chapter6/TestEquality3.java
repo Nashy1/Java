3d4e843b749de1cc02088b82718215bebbdbab57
Nashy1 <nashnyembe0902@gmail.com>
2023-08-08 14:22:37 +0200

    com

--- JavaGroupG/src/Book3/Chapter6/TestEquality3.java
+++ JavaGroupG/src/Book3/Chapter6/TestEquality3.java
@@ -3,10 +3,10 @@ package Book3.Chapter6;
 public class TestEquality3 {
     public static void main(String[] args) {
         Employee emp1 = new Employee("Lot","Jot");
-        Employee emp2 = new Employee("Lot","Jot");
-        
+        Employee2 emp2 = new Employee2("Lot","Jot");
 
-        if (emp1.equals(emp3)){
+
+        if (emp1.equals(emp2)){
             System.out.println("This emp are the same.");
         } else {
             System.out.println("This emp are different");
