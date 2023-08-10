d5fec633eba06d1be89ca106be6d0592d5decccf
Nashy1 <nashnyembe0902@gmail.com>
2023-08-10 15:49:52 +0200

    com

--- JavaGroupG/src/Book4/Chapter2/Enhancedloop.java
+++ JavaGroupG/src/Book4/Chapter2/Enhancedloop.java
@@ -4,15 +4,19 @@ import javax.sound.midi.Soundbank;
 
 public class Enhancedloop {
     public static void main(String[] args) {
-        String[] days = {"Monday","Tuesday","Wednesday","Thursday","Friday"};
-        for(String day: days){
-            System.out.println(day);
-        }
+        String[] days =getDaysOfWeeks();
+        printStringArray(days);
     }
 
     public static String[] getDaysOfWeeks(){
-        String[] days = {"Monday","Tuesday","Wednesday","Thursday","Friday"};
+        String[] days = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
         return days;
+    }
+
+    public static void printStringArray(String[] strings){
+        for(String s: strings){
+            System.out.println(s);
+        }
     }
 
 
