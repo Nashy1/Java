864be63a2ae88a7249ce57ff32ee2b9457698711
Nashy1 <nashnyembe0902@gmail.com>
2023-08-30 11:57:03 +0200

    com

--- JavaGroupG/src/Book4/Chapter4/linkedList1.java
+++ JavaGroupG/src/Book4/Chapter4/linkedList1.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 
 public class linkedList1 {
     public static void main(String[] args) {
+        String jake = "Jake";
         LinkedList<String> officers= new LinkedList<>();
         //add
         officers.add("Mike");
@@ -11,20 +12,38 @@ public class linkedList1 {
         officers.add("Zeke");
         officers.add("Walo");
         //addFirst
-//        officers.addFirst("Blake");
-//        officers.addFirst("Burns");
-//        officers.addFirst("Houlihan");
-//        officers.addFirst("Pierce");
+        officers.addFirst("Blake");
+        officers.addFirst("Burns");
+        officers.addFirst("Houlihan");
+        officers.addFirst("Pierce");
+
         //adding in a specific index
-        officers.add(2,"Jake");
+        officers.add(2,jake);
+
         //updating an index using set
-        String removedItem = officers.set(2,"Murdock");
+//        String removedItem = officers.set(2,"Murdock");
 
 
-        for (String s : officers) {
-            System.out.println(s);
-        }
-        System.out.println("Removed item is: " + removedItem);
+
+
+//        for (String s : officers) {
+//            System.out.println(s);
+//        }
+//        System.out.println("Removed item is: " + removedItem);
+
+
+        //removing
+        System.out.println( "R:"+officers);
+        officers.remove("Mike");
+        System.out.println( "R1:"+officers);
+        officers.remove(4);
+        System.out.println("R2:" +officers);
+        officers.remove((jake));
+        System.out.println("R3:" +officers);
+        officers.remove();
+        System.out.println("R4:" +officers);
+        officers.clear();
+        System.out.println("R5:" +officers);
 
 
     }
