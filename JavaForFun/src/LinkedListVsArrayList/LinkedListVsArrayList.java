ec82bf2027019dbdf31908fac22fe82fd4eaed4f
Nashy1 <nashnyembe0902@gmail.com>
2023-08-30 12:25:54 +0200

    com

--- JavaForFun/src/LinkedListVsArrayList/LinkedListVsArrayList.java
+++ JavaForFun/src/LinkedListVsArrayList/LinkedListVsArrayList.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class LinkedListVsArrayList {
+
+    /*The differences between this two class is how they are implemented behind the
+      scenes and depending on what your specific program has to do. Using one over the other can
+     impact the performance of your program.
+     basically the arrayList uses an array as its underlyiing data structure
+     */
     public static void main(String[] args) {
         LinkedList<String> nameLinkedlist = new LinkedList<>();
         nameLinkedlist.add("Ash");
