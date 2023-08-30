adfdc1bad31a8ca3abb547739e3c32dfe9c4aa01
Nashy1 <nashnyembe0902@gmail.com>
2023-08-30 08:22:54 +0200

    com

---
+++ JavaForFun/src/ArrayVsArrayList/ArrayVsArrayList.java
@@ -0,0 +1,9 @@
+package ArrayVsArrayList;
+
+public class ArrayVsArrayList {
+    public static void main(String[] args) {
+        //THIS IS A ARRAY STRING.Arrays in java have a fixed size,mean after you have declared your array , it cant increase or decrease
+        String[] friendsArray =  new String[3];
+    }
+
+}
