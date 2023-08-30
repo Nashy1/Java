3f122338a7482ef29b6bcc29ad01c909b0b71fe5
Nashy1 <nashnyembe0902@gmail.com>
2023-08-30 12:01:31 +0200

    com

--- JavaForFun/src/LinkedListVsArrayList/LinkedListVsArrayList.java
+++ JavaForFun/src/LinkedListVsArrayList/LinkedListVsArrayList.java
@@ -1,7 +1,20 @@
 package LinkedListVsArrayList;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class LinkedListVsArrayList {
     public static void main(String[] args) {
-        
+        LinkedList<String> nameLinkedlist = new LinkedList<>();
+        nameLinkedlist.add("Ash");
+        nameLinkedlist.add("Rey");
+        nameLinkedlist.add("Edi");
+
+        ArrayList<String> nameArraylist = new ArrayList<>();
+        nameArraylist.add("Jol");
+        nameArraylist.add("Pol");
+        nameArraylist.add("Zol");
+
+
     }
 }
