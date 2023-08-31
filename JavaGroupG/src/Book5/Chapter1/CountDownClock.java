b65a00aea744de03ef94024f52a9d458d64f18b2
Nashy1 <nashnyembe0902@gmail.com>
2023-08-31 13:52:39 +0200

    com

--- JavaGroupG/src/Book5/Chapter1/CountDownClock.java
+++ JavaGroupG/src/Book5/Chapter1/CountDownClock.java
@@ -2,7 +2,7 @@ package Book5.Chapter1;
 
 public class CountDownClock extends Thread {
     public void run(){
-        for (int t = 0; t>=0; t--){
+        for (int t = 20; t >= 0; t--){
             System.out.println("T minus "+t);
             try{
                 Thread.sleep(1000);
