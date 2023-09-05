39d250d70dbbe63794f01b0b546bcb3371f7a553
Nashy1 <nashnyembe0902@gmail.com>
2023-09-05 06:23:16 +0200

    com

--- JavaGroupG/src/Book5/Chapter1/LaunchEvent.java
+++ JavaGroupG/src/Book5/Chapter1/LaunchEvent.java
@@ -1,3 +1,21 @@
 package Book5.Chapter1;
 
-
+public class LaunchEvent implements Runnable{
+    private int start;
+    private String message;
+    public LaunchEvent(int start, String message) →6
+    {
+        this.start = start;
+        this.message = message;
+    }
+    public void run()
+    {
+        try
+        {
+            Thread.sleep(20000 - (start * 1000)); →16
+        }
+        catch (InterruptedException e)
+        {}
+        System.out.println(message); →20
+    }
+}
--- JavaGroupG/src/Book5/Chapter4/FormatDateTime.java
+++ JavaGroupG/src/Book5/Chapter4/FormatDateTime.java
@@ -17,9 +17,8 @@ public class FormatDateTime {
         printDate(now, "h:mm a");
     }
     public static void printDate(LocalDateTime date,String pattern){
-        DateTimeFormatter f;
-        f = DateTimeFormatter.ofPattern(pattern);
-        pattern = (pattern + " ").substring(0,14);
-        System.out.println(pattern+ " " + date.format());
+        DateTimeFormatter f = DateTimeFormatter.ofPattern(pattern);
+        pattern = (pattern + "              ").substring(0,14);
+        System.out.println(pattern+ " " + date.format(f));
     }
 }
