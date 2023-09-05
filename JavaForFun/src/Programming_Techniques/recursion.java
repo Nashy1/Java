e81c80d1eaa56c65492321ad16cbbcce4b9d3cad
Nashy1 <nashnyembe0902@gmail.com>
2023-09-05 08:12:50 +0200

    com

--- JavaForFun/src/Programming_Techniques/recursion.java
+++ JavaForFun/src/Programming_Techniques/recursion.java
@@ -1,4 +1,15 @@
 package Programming_Techniques;
 
 public class recursion {
+    /*recursion is when you have a method that calls its self*/
+    public static void main(String[] args) {
+        sayHi();
+
+    }
+    private static void sayHi(){
+        System.out.println("Hi");
+        /*to make this method recursive, we will call it in of its self*/
+        sayHi();
+    }
+
 }
