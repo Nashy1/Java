09b834c0e7f497e7718c367ecce0938b2bf92de8
Nashy1 <nashnyembe0902@gmail.com>
2023-09-05 13:36:45 +0200

    com

---
+++ Java_DummiesBook6/src/main/java/com/example/java_dummiesbook6/Chapter2/Annon.java
@@ -0,0 +1,59 @@
+package com.example.java_dummiesbook6.Chapter2;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class Annon extends Application {
+
+
+        Button btnAdd;
+        Button btnSub;
+        Label lbl;
+        int iCounter;
+
+        public static void main(String[] args) {
+            launch(args);
+        }
+
+        @Override
+        public void start(Stage primaryStage) {
+            //instance of Clickhandler
+            com.example.java_dummiesbook6.Chapter2.InnerClass.ClickHandler ch = new com.example.java_dummiesbook6.Chapter2.InnerClass.ClickHandler();
+            //Add btn setup
+            btnAdd = new Button();
+            btnAdd.setText("Add");
+            btnAdd.setOnAction(new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent e)
+                {
+                    iCounter++;
+                    lbl.setText(Integer.toString(iCounter));
+                }
+            } );
+
+            //Sub btn setup
+            btnSub = new Button();
+            btnSub.setText("Subtract");
+            btnSub.setOnAction(ch);
+
+            //label setup
+            lbl=new Label();
+            lbl.setText(Integer.toString(iCounter));
+
+            //pane setup
+            HBox pane = new HBox(10);
+            pane.getChildren().addAll(lbl,btnAdd,btnSub);
+
+            //scene setup
+            Scene scene =new Scene(pane,200,75);
+
+            //stage setup
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Add/Subtract");
+            primaryStage.show();
+        }
+
+}
