69c2137b2278560a7dbec4e1b9a8acc7e6478f7a
Nashy1 <nashnyembe0902@gmail.com>
2023-09-05 06:26:24 +0200

    com

--- JavaGroupG/src/Book5/Chapter1/CountDownApp2.java
+++ JavaGroupG/src/Book5/Chapter1/CountDownApp2.java
@@ -1,4 +1,16 @@
 package Book5.Chapter1;
 
 public class CountDownApp2 {
+    public static void main(String[] args)
+    {
+        Thread clock = new CountDownClock(); →5
+        Runnable flood, ignition, liftoff; →7
+        flood = new LaunchEvent(16, "Flood the pad!");
+        ignition = new LaunchEvent(6, "Start engines!");
+        liftoff = new LaunchEvent(0, "Liftoff!");
+        clock.start(); →12
+        new Thread(flood).start(); →14
+        new Thread(ignition).start();
+        new Thread(liftoff).start();
+    }
 }
