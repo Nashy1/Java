9e74d1c262acb23f614bb1909debed9a25dc2995
Nashy1 <nashnyembe0902@gmail.com>
2023-09-06 07:50:45 +0200

    com

--- Java_DummiesBook6/src/main/java/com/example/java_dummiesbook6/Chapter1/ClickeMe.java
+++ Java_DummiesBook6/src/main/java/com/example/java_dummiesbook6/Chapter1/ClickeMe.java
@@ -1,16 +1,43 @@
 package com.example.java_dummiesbook6.Chapter1;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class ClickeMe extends Application {
-
-    public static void main(String[] args) {
+public class ClickeMe extends Application
+{
+    public static void main(String[] args)
+    {
         launch(args);
     }
-
-    @Override
-    public void start(Stage primaryStage) {
-
+    Button btn;
+    @Override public void start(Stage primaryStage)
+    {
+// Create the button
+        btn = new Button();
+        btn.setText("Click me please!");
+        btn.setOnAction(e -> buttonClick());
+// Add the button to a layout pane
+        BorderPane pane = new BorderPane();
+        pane.setCenter(btn);
+// Add the layout pane to a scene
+        Scene scene = new Scene(pane, 300, 250);
+// Finalize and show the stage
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("The Click Me App");
+        primaryStage.show();
+    }
+    public void buttonClick()
+    {
+        if (btn.getText() == "Click me please!")
+        {
+            btn.setText("You clicked me!");
+        }
+        else
+        {
+            btn.setText("Click me please!");
+        }
     }
 }
