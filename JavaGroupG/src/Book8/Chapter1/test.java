d5f0e65cc0a0d7839ad36b60cf471a48b6681d93
Nashy1 <nashnyembe0902@gmail.com>
2023-09-11 15:10:48 +0200

    com

---
+++ JavaGroupG/src/Book8/Chapter1/test.java
@@ -0,0 +1,9 @@
+package Book8.Chapter1;
+
+public class test {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
