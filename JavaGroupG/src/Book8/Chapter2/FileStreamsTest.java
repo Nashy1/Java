cdbba154d53a68803f5c0f147d9d377ad82511e4
Nashy1 <nashnyembe0902@gmail.com>
2023-09-12 13:05:06 +0200

    com

--- JavaGroupG/src/Book8/Chapter2/FileStreamsTest.java
+++ JavaGroupG/src/Book8/Chapter2/FileStreamsTest.java
@@ -1,7 +1,23 @@
 package Book8.Chapter2;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class FileStreamsTest {
     public static void main(String[] args) {
+        try {
+            File f = new File("Movies.txt");
+            BufferedReader in = new BufferedReader(new FileReader(f));
+            String line = in.readLine();
+            while (line != null){
+                System.out.println(line);
+                line = in.readLine();
+            }
+        }catch (Exception e){
+            System.out.println("Error" +e);
+        }
+
 
     }
 }
