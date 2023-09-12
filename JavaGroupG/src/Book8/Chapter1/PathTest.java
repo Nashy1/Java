03cb758695d8286a3b98ce40910e3c71511e6803
Nashy1 <nashnyembe0902@gmail.com>
2023-09-12 11:24:14 +0200

    com

--- JavaGroupG/src/Book8/Chapter1/PathTest.java
+++ JavaGroupG/src/Book8/Chapter1/PathTest.java
@@ -8,7 +8,7 @@ import java.nio.file.Paths;
 
 public class PathTest {
     public static void main(String[] args) {
-        Path p =  Paths.get("C:\\Users\\ghost\\OneDrive\\Documents\\HelloApp7.java");
+
 //        if(!Files.exists(p)){
 //            System.out.println("File exists");
 //        }
@@ -23,11 +23,11 @@ public class PathTest {
 //        }
 
 
-
+        Path p =  Paths.get("C:\\");
         try {
-            DirectoryStream<Path> stream = Files.newDirectoryStream(p);
+            DirectoryStream<Path> stream = Files.newDirectoryStream(p,"*.txt");
             for(Path entry: stream){
-                System.out.println(entry.toString());
+                System.out.println(entry);
             }
         }catch (Exception e){
             System.out.println("Error: " +e);
