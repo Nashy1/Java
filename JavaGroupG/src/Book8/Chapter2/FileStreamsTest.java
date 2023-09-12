27e5676f1a5b4fa5e19a5b69d7dca7ab8bcf2b07
Nashy1 <nashnyembe0902@gmail.com>
2023-09-12 13:06:11 +0200

    com

--- JavaGroupG/src/Book8/Chapter2/FileStreamsTest.java
+++ JavaGroupG/src/Book8/Chapter2/FileStreamsTest.java
@@ -7,7 +7,7 @@ import java.io.FileReader;
 public class FileStreamsTest {
     public static void main(String[] args) {
         try {
-            File f = new File("Movies.txt");
+            File f = new File("C:\\Users\\ghost\\IdeaProjects\\JavaGroupG\\src\\Book8\\Chapter2\\Movies.txt");
             BufferedReader in = new BufferedReader(new FileReader(f));
             String line = in.readLine();
             while (line != null){
