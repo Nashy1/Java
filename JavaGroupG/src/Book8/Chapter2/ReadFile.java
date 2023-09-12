d4546ba16e4333ad2cf5eadcb1debf9b274cdf7c
Nashy1 <nashnyembe0902@gmail.com>
2023-09-12 13:26:11 +0200

    com

---
+++ JavaGroupG/src/Book8/Chapter2/ReadFile.java
@@ -0,0 +1,35 @@
+package Book8.Chapter2;
+
+import java.io.*;
+import java.text.NumberFormat;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        NumberFormat cf = NumberFormat.getCurrencyInstance();
+        BufferedReader in = getReader("Movies.txt");
+
+    }
+    private static BufferedReader getReader(String name){
+        BufferedReader in = null;
+        try{
+            File file = new File(name);
+            in = new BufferedReader(new FileReader(file));
+        }catch (FileNotFoundException e){
+            System.out.println("The file doesn't exist.");
+            System.exit(0);
+        }
+        return in;
+    }
+    private static Movie readMovie(BufferedReader in){
+        String title;
+        int year;
+        double price;
+        String line = "";
+        String[] data;
+        try {
+            line= in.readLine();
+        }catch (IOException e){
+            
+        }
+    }
+}
