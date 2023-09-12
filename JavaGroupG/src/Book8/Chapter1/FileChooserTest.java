0e59f90e87fae8fac7d1d7d3dc4c45a659acd86d
Nashy1 <nashnyembe0902@gmail.com>
2023-09-12 09:23:27 +0200

    com

---
+++ JavaGroupG/src/Book8/Chapter1/FileChooserTest.java
@@ -0,0 +1,23 @@
+package Book8.Chapter1;
+
+import javax.swing.*;
+import java.io.File;
+
+public class FileChooserTest {
+    public static void main(String[] args) {
+//        FileChooserTest tc = new FileChooserTest();
+        System.out.println("You chose this file:" + getFile());
+    }
+
+    static File getFile() {
+        JFileChooser fc = new JFileChooser();
+        int result = fc.showOpenDialog(null);
+        File file = null;
+        if (result == JFileChooser.APPROVE_OPTION) {
+            file = fc.getSelectedFile();
+        }  return file;
+
+    }
+}
+
+
