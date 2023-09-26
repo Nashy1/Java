963012a3e52da31f6abaf9ad5798c63834dd2dfe
Nashy1 <nashnyembe0902@gmail.com>
2023-09-26 12:40:10 +0200

    com

--- JavaGroupG/src/OCPBook/Chapter5/BoxingAndUnboxing.java
+++ JavaGroupG/src/OCPBook/Chapter5/BoxingAndUnboxing.java
@@ -1,4 +1,18 @@
 package OCPBook.Chapter5;
 
 public class BoxingAndUnboxing {
+    public static void main(String[] args) {
+        int quack =5;
+        Integer quackquack = quack; //Autoboxing
+        int quackquackquack =quack; //Unboxing
+
+        Short tail =8;              //Autoboxing
+        Character p = Character.valueOf('p');
+        char paw = p;              //Unboxing
+        Boolean nose = true;      //Autoboxing
+        Integer e = Integer.valueOf(9);
+        long ears =e;             // Unboxing, then implicit casting
+
+    }
+
 }
