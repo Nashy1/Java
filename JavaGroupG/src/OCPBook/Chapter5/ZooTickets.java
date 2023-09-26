d483e558025d2f73b44642590bdf0e0d7b52e791
Nashy1 <nashnyembe0902@gmail.com>
2023-09-26 12:05:02 +0200

    com

--- JavaGroupG/src/OCPBook/Chapter5/ZooTickets.java
+++ JavaGroupG/src/OCPBook/Chapter5/ZooTickets.java
@@ -1,4 +1,21 @@
 package OCPBook.Chapter5;
 
 public class ZooTickets {
+    public static void main(String[] args) {
+        int tickets =2;                //tickets = 2
+        String guests ="abc";          //guests =abc
+        addTickets(tickets);           //tickets = 2 because we have not assigned the method to tickets
+//        tickets = addTickets(tickets);
+        guests =addGuest(guests);      //guests = abcd
+        System.out.println(tickets + guests);  //2abcd
+    }
+    public static int addTickets(int tickets){
+        tickets++;
+        return tickets;
+    }
+    public static String addGuest(String guests){
+        guests += "d";
+        return guests;
+    }
+
 }
