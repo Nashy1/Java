674b8823e5eca1da13bf816100c0f40789f3ae04
Nashy1 <nashnyembe0902@gmail.com>
2023-09-26 14:59:08 +0200

    com

---
+++ JavaGroupG/src/Chapter6/Bunny.java
@@ -0,0 +1,12 @@
+package Chapter6;
+
+public class Bunny {
+    public Bunny(){
+        System.out.println("hop");
+    }
+
+    public static void main(String[] args) {
+        var b = new Bunny();
+        
+    }
+}
