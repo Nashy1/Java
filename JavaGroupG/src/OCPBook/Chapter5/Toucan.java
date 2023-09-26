77e30e77b0bf9beffbc586dd36375ca47712d010
Nashy1 <nashnyembe0902@gmail.com>
2023-09-26 13:39:20 +0200

    com

---
+++ JavaGroupG/src/OCPBook/Chapter5/Toucan.java
@@ -0,0 +1,18 @@
+package OCPBook.Chapter5;
+
+import java.awt.*;
+
+public class Toucan {
+//    public void fly(int[] lengths){
+//        System.out.println("array of int");
+//    }
+    public void fly(int... lengths){
+        System.out.println("array of int");
+    }
+
+    public static void main(String[] args) {
+        Toucan t =new Toucan();
+//        t.fly(new int[]{1,2,3});
+        t.fly(1,2,3);
+    }
+}
