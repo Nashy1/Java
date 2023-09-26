a49a9c54a039f3082ef9d149bc972117bf32de1b
Nashy1 <nashnyembe0902@gmail.com>
2023-09-26 14:03:32 +0200

    com

--- JavaGroupG/src/Chapter6/BigCat.java
+++ JavaGroupG/src/Chapter6/BigCat.java
@@ -2,6 +2,14 @@ package Chapter6;
 
 public class BigCat {
     protected double size;
+
+    public static void main(String[] args) {
+        Jaguar j =new Jaguar();
+        j.printDetails();
+
+        Spider s =new Spider();
+        s.printDetail();
+    }
 }
 
  class Jaguar extends BigCat{
@@ -11,11 +19,9 @@ public class BigCat {
     public void printDetails(){
         System.out.println(size); // a child class will have access to protected and public member of the parent just like here
     }
-
-    class Spider{
-        public void printDetail(){
+}
+class Spider{
+public void printDetail(){
 //            System.out.println(size); // won't compile because it's not a child class
         }
-    }
-
 }
