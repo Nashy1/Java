b66be7641afd70702db835a2c275f59cb0c11f31
Nashy1 <nashnyembe0902@gmail.com>
2023-09-26 14:26:43 +0200

    com

--- JavaGroupG/src/Chapter6/Aniaml.java
+++ JavaGroupG/src/Chapter6/Aniaml.java
@@ -1,4 +1,30 @@
 package Chapter6;
 
 public class Aniaml {
+    private int age;
+    protected String name;
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int newAge){
+        age =newAge;
+    }
+}
+
+class lion extends Aniaml{
+    protected void setProperties(int age,String n){
+        setAge(age);
+        name =n;
+    }
+
+    public void roar(){
+        System.out.println(name + ", age: " + getAge() + ", says: Roar!");
+    }
+
+    public static void main(String[] args) {
+        var lion = new lion();
+        lion.setProperties(3,"Kion");
+        lion.roar();
+    }
+
 }
