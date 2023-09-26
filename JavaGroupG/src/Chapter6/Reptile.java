dbd653c1210f4dce8b2da40a49531005534cc7a2
Nashy1 <nashnyembe0902@gmail.com>
2023-09-26 14:46:24 +0200

    com

--- JavaGroupG/src/Chapter6/Reptile.java
+++ JavaGroupG/src/Chapter6/Reptile.java
@@ -1,4 +1,17 @@
 package Chapter6;
 
 public class Reptile {
+    protected int speed = 10;
+}
+
+class Crocodile extends Reptile{
+    protected int speed =20;
+    public int getSpeed(){
+        return speed;
+    }
+
+    public static void main(String[] args) {
+        var croc = new Crocodile();
+        System.out.println(croc.getSpeed());
+    }
 }
